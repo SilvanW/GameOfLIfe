67530fd03aaf997344c46811c8134c4120c574dd
SilvanW <silvan.wiedmer@bluewin.ch>
2026-09-19 17:54:26 +0200

    add numba compiled version which is only slightly faster

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import time
+
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.animation import FFMpegWriter
 from tqdm import tqdm
 
-from src.rules import apply_rules
+from src.rules import apply_rules, apply_rules_numba
 
 """
 Rules
@@ -17,7 +19,7 @@ Rules
 grid = np.zeros((50, 50), dtype=np.int8)
 grid_copy = np.zeros_like(grid)
 
-num_generations = 500
+num_generations = 50
 
 
 # TODO: fine a nice way to add those programatically
@@ -54,14 +56,20 @@ im = ax.imshow(grid)
 # Create video
 with writer.saving(fig, "output.mp4", dpi=600):
     writer.grab_frame()
+    start = time.perf_counter()
     for generation in tqdm(range(num_generations)):
-        apply_rules(grid, grid_copy)
+        # apply_rules(grid, grid_copy)
+        apply_rules_numba(grid, grid_copy)
 
         grid = grid_copy.copy()
         grid_copy[:, :] = 0
 
         im.set_data(grid)  # Update the data
         writer.grab_frame()  # Capture the frame
+
+    end = time.perf_counter()
+
+print(f"Elapsed: {end - start:.6f} s")
 
 plt.imshow(grid)
 plt.title("Game of Life Result")
--- src/rules.py
+++ src/rules.py
@@ -1,39 +1,59 @@
 import numpy as np
+from numba import jit
 
-MOORE_CELL_OFFSETS: list[tuple[int, int]] = [
-    (0, 1),
-    (1, 1),
-    (1, 0),
-    (1, -1),
-    (0, -1),
-    (-1, -1),
-    (-1, 0),
-    (-1, 1),
-]
+MOORE_CELL_OFFSETS: np.ndarray = np.array(
+    [
+        (0, 1),
+        (1, 1),
+        (1, 0),
+        (1, -1),
+        (0, -1),
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+    ],
+    dtype=np.int8,
+)
 
 
 def apply_rules(grid: np.ndarray, grid_copy: np.ndarray) -> None:
-    for (j, i), value in np.ndenumerate(grid[1:-1, 1:-1]):
-        # Required due to grid slicing in enumerate
-        j += 1
-        i += 1
+    rows, cols = grid.shape
+    for j in range(1, rows - 1):
+        for i in range(1, cols - 1):
+            value = grid[j, i]
 
-        if value == 0:
             num_alive = 0
             for dy, dx in MOORE_CELL_OFFSETS:
                 if grid[j + dy, i + dx] == 1:
                     num_alive += 1
 
-            if num_alive == 3:
+            if value == 0 and num_alive == 3:
                 grid_copy[j, i] = 1
 
-        if value == 1:
+            if value == 1:
+                if num_alive == 2 or num_alive == 3:
+                    grid_copy[j, i] = 1
+                else:
+                    grid_copy[j, i] = 0
+
+
+@jit(nopython=True)
+def apply_rules_numba(grid: np.ndarray, grid_copy: np.ndarray) -> None:
+    rows, cols = grid.shape
+    for j in range(1, rows - 1):
+        for i in range(1, cols - 1):
+            value = grid[j, i]
+
             num_alive = 0
             for dy, dx in MOORE_CELL_OFFSETS:
                 if grid[j + dy, i + dx] == 1:
                     num_alive += 1
 
-            if num_alive == 2 or num_alive == 3:
+            if value == 0 and num_alive == 3:
                 grid_copy[j, i] = 1
-            else:
-                grid_copy[j, i] = 0
+
+            if value == 1:
+                if num_alive == 2 or num_alive == 3:
+                    grid_copy[j, i] = 1
+                else:
+                    grid_copy[j, i] = 0
