b560a45202d0b78192fec8b61afb6d72433e0db0
SilvanW <silvan.wiedmer@bluewin.ch>
2026-09-19 17:38:04 +0200

    add tqdm progress bar

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.animation import FFMpegWriter
+from tqdm import tqdm
 
 from src.rules import apply_rules
 
@@ -16,20 +17,27 @@ Rules
 grid = np.zeros((50, 50), dtype=np.int8)
 grid_copy = np.zeros_like(grid)
 
-num_generations = 5
+num_generations = 500
 
 
 # TODO: fine a nice way to add those programatically
 # Still life
-grid[5, 5] = 1
-grid[5, 6] = 1
-grid[6, 5] = 1
-grid[6, 6] = 1
+# grid[5, 5] = 1
+# grid[5, 6] = 1
+# grid[6, 5] = 1
+# grid[6, 6] = 1
 
 # Flipper
-grid[20, 20] = 1
-grid[20, 21] = 1
-grid[20, 22] = 1
+# grid[20, 20] = 1
+# grid[20, 21] = 1
+# grid[20, 22] = 1
+
+# Acorn
+grid[25, 25] = 1
+grid[25, 26] = 1
+grid[23, 26] = 1
+grid[24, 28] = 1
+grid[25, 29:32] = 1
 
 plt.imshow(grid)
 plt.title("Game of Life Initial State")
@@ -45,7 +53,8 @@ im = ax.imshow(grid)
 
 # Create video
 with writer.saving(fig, "output.mp4", dpi=600):
-    for generation in range(num_generations):
+    writer.grab_frame()
+    for generation in tqdm(range(num_generations)):
         apply_rules(grid, grid_copy)
 
         grid = grid_copy.copy()
