33cb5f27447fd3034b2b1251d50223e99628dec5
SilvanW <silvan.wiedmer@bluewin.ch>
2026-09-19 11:28:50 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+"""
+Rules
+	- cell c is 0 (dead)
+		- if 3 neighbours are 1 then => 1
+	- call c is 1 (alive)
+		- if 2 or 3 alive neighbours -> then 1 (no change)
+		- else: -> 0 (dead)
+"""
+
+grid = np.zeros((50, 50), dtype=np.int8)
+grid_copy = np.zeros_like(grid)
+
+num_generations = 1
+
+
+def get_moore_neighbourhood_cell_offsets() -> list[tuple[int, int]]:
+    return [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)]
+
+
+grid[5, 5] = 1
+grid[5, 6] = 1
+grid[6, 5] = 1
+grid[6, 6] = 1
+
+plt.imshow(grid)
+plt.title("Game of Life Initial State")
+plt.show()
+
+for generation in range(num_generations):
+    for (j, i), value in np.ndenumerate(grid[1:-1, 1:-1]):
+        if value == 0:
+            num_alive = 0
+            for dy, dx in get_moore_neighbourhood_cell_offsets():
+                if grid[j + dy, i + dx] == 1:
+                    num_alive += 1
+
+            if num_alive == 3:
+                grid_copy[j, i] = 1
+
+        if value == 1:
+            num_alive = 0
+            for dy, dx in get_moore_neighbourhood_cell_offsets():
+                if grid[j + dy, i + dx] == 1:
+                    num_alive += 1
+
+            if num_alive == 2 or num_alive == 3:
+                grid_copy[j, i] = 1
+            else:
+                grid_copy[j, i] = 0
+
+    grid = grid_copy.copy()
+    grid_copy[:, :] = 0
+
+print(np.unique(grid))
+
+plt.imshow(grid)
+plt.title("Game of Life Result")
+plt.show()
